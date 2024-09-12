3fefaaf5f33a41856ca5ac7230c8daac9fa7051b
BJG <bjg@email.com>
2024-09-12 10:31:15 +1000

    Setup Tailwind (v4 alpha)

--- app/root.jsx
+++ app/root.jsx
@@ -18,6 +18,7 @@ import {
 import favicon from '~/assets/favicon.svg';
 import resetStyles from '~/styles/reset.css?url';
 import appStyles from '~/styles/app.css?url';
+import tailwindCss from './styles/tailwind.css?url';
 import {PageLayout} from '~/components/PageLayout';
 import {FOOTER_QUERY, HEADER_QUERY} from '~/lib/fragments';
 import {seoPayload} from '~/lib/seo';
@@ -43,6 +44,7 @@ export const shouldRevalidate = ({
 
 export function links() {
   return [
+    {rel: 'stylesheet', href: tailwindCss},
     {rel: 'stylesheet', href: resetStyles},
     {rel: 'stylesheet', href: appStyles},
     {
